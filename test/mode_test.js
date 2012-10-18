015688c2c2d223490d446ad81639aaafe1fa6d86
Roffis Baetz <roffis@roffiscode.org>
2012-10-17 20:02:43 -0500

    First New Commit to File. Add New Stuff!
    Signed-off-by: Roffis Baetz <roffis@roffiscode.org>

--- test/mode_test.js
+++ test/mode_test.js
@@ -10,49 +10,33 @@ ModeTest.modeOptions = {};
 ModeTest.modeName = CodeMirror.defaults.mode;
 
 /* keep track of results for printSummary */
-ModeTest.testCount = 0;
+ModeTest.tests = 0;
 ModeTest.passes = 0;
 
 /**
  * Run a test; prettyprints the results using document.write().
- * 
- * @param name Name of test
- * @param text String to highlight.
- * @param expected Expected styles and tokens: Array(style, token, [style, token,...])
- * @param modeName
- * @param modeOptions
- * @param expectedFail
+ *
+ * @param string to highlight
+ *
+ * @param style[i] expected style of the i'th token in string
+ *
+ * @param token[i] expected value for the i'th token in string
  */
-ModeTest.testMode = function(name, text, expected, modeName, modeOptions, expectedFail) {
-  ModeTest.testCount += 1;
-  
-  if (!modeName) modeName = ModeTest.modeName;
-  
-  if (!modeOptions) modeOptions = ModeTest.modeOptions;
+ModeTest.test = function() {
+  ModeTest.tests += 1;
 
-  var mode = CodeMirror.getMode(modeOptions, modeName);
+  var mode = CodeMirror.getMode(ModeTest.modeOptions, ModeTest.modeName);
 
-  if (expected.length < 0) {
-    throw "must have text for test (" + name + ")";
+  if (arguments.length < 1) {
+    throw "must have text for test";
   }
-  if (expected.length % 2 != 0) {
-    throw "must have text for test (" + name + ") plus expected (style, token) pairs";
+  if (arguments.length % 2 != 1) {
+    throw "must have text for test plus expected (style, token) pairs";
   }
-  return test(
-    modeName + "_" + name,
-    function(){
-      return ModeTest.compare(text, expected, mode);
-    },
-    expectedFail
-  );
-  
-}
 
-ModeTest.compare = function (text, arguments, mode) {
-
+  var text = arguments[0];
   var expectedOutput = [];
-  for (var i = 0; i < arguments.length; i += 2) {
-    arguments[i] = (arguments[i] != null ? arguments[i].split(' ').sort().join(' ') : arguments[i]);
+  for (var i = 1; i < arguments.length; i += 2) {
     expectedOutput.push([arguments[i],arguments[i + 1]]);
   }
 
@@ -66,26 +50,20 @@ ModeTest.compare = function (text, arguments, mode) {
   }
 
   var s = '';
+  s += '<div class="mt-test ' + passStyle + '">';
+  s +=   '<pre>' + ModeTest.htmlEscape(text) + '</pre>';
+  s +=   '<div class="cm-s-default">';
   if (pass || expectedOutput.length == 0) {
-    s += '<div class="mt-test ' + passStyle + '">';
-    s +=   '<pre>' + ModeTest.htmlEscape(text) + '</pre>';
-    s +=   '<div class="cm-s-default">';
     s +=   ModeTest.prettyPrintOutputTable(observedOutput);
-    s +=   '</div>';
-    s += '</div>';
-    return s;
   } else {
-    s += '<div class="mt-test ' + passStyle + '">';
-    s +=   '<pre>' + ModeTest.htmlEscape(text) + '</pre>';
-    s +=   '<div class="cm-s-default">';
     s += 'expected:';
     s +=   ModeTest.prettyPrintOutputTable(expectedOutput);
     s += 'observed:';
     s +=   ModeTest.prettyPrintOutputTable(observedOutput);
-    s +=   '</div>';
-    s += '</div>';
-    throw s;
   }
+  s +=   '</div>';
+  s += '</div>';
+  document.write(s);
 }
 
 /**
@@ -107,27 +85,11 @@ ModeTest.highlight = function(string, mode) {
     var line = lines[i];
     var stream = new CodeMirror.StringStream(line);
     if (line == "" && mode.blankLine) mode.blankLine(state);
-    var pos = 0;
-    var st = [];
-    /* Start copied code from CodeMirror.highlight */
     while (!stream.eol()) {
-      var style = mode.token(stream, state), substr = stream.current();
+      var style = mode.token(stream, state);
+      var substr = line.slice(stream.start, stream.pos);
+      output.push([style, substr]);
       stream.start = stream.pos;
-      if (pos && st[pos-1] == style) {
-        st[pos-2] += substr;
-      } else if (substr) {
-        st[pos++] = substr; st[pos++] = style;
-      }
-      // Give up when line is ridiculously long
-      if (stream.pos > 5000) {
-        st[pos++] = this.text.slice(stream.pos); st[pos++] = null;
-        break;
-      }
-    }
-    /* End copied code from CodeMirror.highlight */
-    for (var x = 0; x < st.length; x += 2) {
-      st[x + 1] = (st[x + 1] != null ? st[x + 1].split(' ').sort().join(' ') : st[x + 1]);
-      output.push([st[x + 1], st[x]]);
     }
   }
 
@@ -169,7 +131,7 @@ ModeTest.prettyPrintOutputTable = function(output) {
     var token = output[i];
     s +=
       '<td class="mt-token">' +
-        '<span class="cm-' + String(token[0]).replace(/ +/g, " cm-") + '">' +
+        '<span class="cm-' + token[0] + '">' +
           ModeTest.htmlEscape(token[1]).replace(/ /g,'&middot;') +
         '</span>' +
       '</td>';
@@ -188,8 +150,7 @@ ModeTest.prettyPrintOutputTable = function(output) {
  * Print how many tests have run so far and how many of those passed.
  */
 ModeTest.printSummary = function() {
-  ModeTest.runTests(ModeTest.displayTest);
-  document.write(ModeTest.passes + ' passes for ' + ModeTest.testCount + ' tests');
+  document.write(ModeTest.passes + ' passes for ' + ModeTest.tests + ' tests');
 }
 
 /**
